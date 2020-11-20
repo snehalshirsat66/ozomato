982ad43d7e5b877caced2299b98bb57c4694e152
Gayatree <gayatree.shirsat@happiestminds.com>
2020-11-20 21:26:29 +0530

    Added project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Restaurant from "./components/Restaurant";
 
 function App() {
+  const [restaurant, setRest] = useState([]);
+  const [id, setId] = useState();
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState([0, ""]);
+
+  useEffect(() => {
+    getRestaurants();
+  }, [query]);
+
+  const getRestaurants = async () => {
+    if (!(query[0] === 0 && query[1] === "")) {
+      const response = await fetch(
+        `https://developers.zomato.com/api/v2.1/search?entity_id=${query[0]}&entity_type=city&q=${query[1]}&count=100`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "user-key": "ebce07efec80d0017e1a3ee7173cdbd6",
+          },
+        }
+      );
+      const data = await response.json();
+      setRest(data.restaurants);
+    }
+  };
+
+  const updateID = (e) => {
+    setId(e.target.value);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery([id, search]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,500&display=swap"
+        rel="stylesheet"
+      />
+      <div className="main">
+        <h1>Find the best Restaurants in your city</h1>
+        <form className="searchBar" onSubmit={getSearch}>
+          <div>
+            <select
+              id="select_id"
+              className="city form-control"
+              onChange={updateID}
+            >
+              <option value="0" hidden>
+                Select City
+              </option>
+              <option value="4">Bengaluru</option>
+              <option value="3">Mumbai</option>
+              <option value="1">Delhi</option>
+              <option value="6">Hyderabad</option>
+              <option value="5">Pune</option>
+              <option value="11290">Trivandrum</option>
+              <option value="7">Chennai</option>
+              <option value="11">Ahmedabad</option>
+            </select>
+            <input
+              id="getText"
+              type="text"
+              className="restaurant"
+              placeholder="Search for Restaurant"
+              onChange={updateSearch}
+            />
+            <button id="getMessage" className="search-btn">
+              Get Restaurants
+            </button>
+          </div>
+        </form>
+      </div>
+
+      {restaurant.map((rest) => (
+        <Restaurant
+          key={rest.restaurant.id}
+          name={rest.restaurant.name}
+          thumb={rest.restaurant.thumb}
+          locality={rest.restaurant.location.locality}
+          address={rest.restaurant.location.address}
+          latitude={rest.restaurant.location.latitude}
+          longitude={rest.restaurant.location.longitude}
+          cuisines={rest.restaurant.cuisines}
+          cost={rest.restaurant.average_cost_for_two}
+          rating={rest.restaurant.user_rating.aggregate_rating}
+          votes={rest.restaurant.user_rating.votes}
+          rating_color={rest.restaurant.user_rating.rating_obj.bg_color.type}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/components/Restaurant.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Restaurant = ({
+  key,
+  name,
+  thumb,
+  locality,
+  address,
+  latitude,
+  longitude,
+  cuisines,
+  cost,
+  rating,
+  votes,
+  rating_color,
+}) => {
+  return (
+    <div className="restaurant-tile">
+      <img alt="" src={thumb} className="rest-img" />
+      <div className="rest-rating">
+        <p style={{ backgroundColor: `${rating_color}` }}>{rating}</p>
+        <div>{votes} votes</div>
+      </div>
+      <div className="rest-details">
+        <h2 className="rest-name">{name}</h2>
+        <div className="rest-loc">{locality}</div>
+        <div className="rest-add">Address - {address}</div>
+        <div className="rest-lat-long">Latitude - {latitude}</div>
+        <div className="rest-lat-long">Longitute - {longitude}</div>
+        <hr />
+        <div className="rest-cusines">
+          <strong>CUISINES:</strong> {cuisines}
+        </div>
+        <div className="rest-cost">
+          <strong>COST FOR TWO:</strong> Rs.{cost}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Restaurant;
